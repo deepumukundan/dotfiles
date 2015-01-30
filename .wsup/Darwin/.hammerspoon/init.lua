14db3f262fb9427781edb4936a51d433380a4bbb
Deepu Mukundan <deepumukundan@gmail.com>
2015-01-30 09:09:37 -0800

    Removed Mjolnir and added HammerSpoon for automated window and hotkey management

---
+++ .wsup/Darwin/.hammerspoon/init.lua
@@ -0,0 +1,178 @@
+local windowConfigs = require "windowConfigs"
+
+---------------------------------- Core Config -----------------------------------
+-- Ensure the IPC command line client is available
+hs.ipc.cliInstall()
+
+-- Set the style of hints
+-- hs.hints.fontName = "SanFranciscoDisplay-Medium"
+-- hs.hints.style = "vimperator"
+-- hs.fnutil.map(hs.window.visibleWindows(), ensureIsInScreenBounds) end) -- TODO - Fix this so that after arrangement windows are not extending
+-- hs.hotkey.bind(hyper, ';', function() hs.fnutil.map(hs.window.visibleWindows(), hs.grid.snap) end) --TODO - Verify
+
+----------------------------------- Variables ------------------------------------
+-- Things we need to clean up at reload
+local configFileWatcher = nil
+local appWatcher = nil
+local wifiWatcher = nil
+local screenWatcher = nil
+
+-- Define some keyboard modifier variables
+-- (Node: Capslock disabled normal function and bound to cmd+alt+ctrl+shift via Seil and Karabiner)
+local alt = {"⌥"}
+local cmd = {"⌘"}
+local hyper = {"⌘", "⌥", "⌃", "⇧"}
+
+-- Define monitor names for layout purposes
+local display_laptop = hs.screen.allScreens()[1]
+local display_middle = hs.screen.allScreens()[2]
+local display_left = hs.screen.allScreens()[3]
+
+-- Defines for screen watcher
+local lastNumberOfScreens = #hs.screen.allScreens()
+
+-- Define window layouts
+--   Format reminder:
+--     {"App name", "Window name", "Display Name", "unitrect", "framerect", "fullframerect"},
+
+local internal_display = {
+    {"iTerm",             nil,          display_laptop, hs.layout.maximized, nil, nil},
+    {"Microsoft Outlook", nil,          display_laptop, hs.layout.maximized, nil, nil},
+    {"Google Chrome",     nil,          display_laptop, hs.layout.left30,    nil, nil},
+    {"TextWrangler",      nil,          display_laptop, hs.layout.maximized, nil, nil},
+    {"Xcode",             nil,          display_laptop, hs.layout.maximized, nil, nil},
+    {"Atom",              nil,          display_laptop, hs.layout.maximized, nil, nil},
+    {"HipChat",           nil,          display_laptop, hs.layout.maximized, nil, nil},
+    {"SourceTree",        nil,          display_laptop, hs.layout.maximized, nil, nil},
+    {"Calendar",          nil,          display_laptop, hs.layout.maximized, nil, nil},
+    {"Evernote",          nil,          display_laptop, hs.layout.maximized, nil, nil},
+    {"Spotify",           nil,          display_laptop, hs.layout.maximized, nil, nil},    
+    {"iTunes",            "iTunes",     display_laptop, hs.layout.maximized, nil, nil},
+    {"Finder",            nil,          display_laptop, hs.layout.maximized, nil, nil},
+}
+
+local triple_display = {
+    {"iTerm",             nil,          display_left,    hs.layout.left50,    nil, nil},
+    {"Microsoft Outlook", nil,          display_left,    hs.layout.right50,   nil, nil},
+    {"Google Chrome",     nil,          display_middle,  hs.layout.maximized, nil, nil},
+    {"TextWrangler",      nil,          display_middle,  hs.layout.maximized, nil, nil},
+    {"Xcode",             nil,          display_middle,  hs.layout.maximized, nil, nil},
+    {"Atom",              nil,          display_laptop,  hs.layout.maximized, nil, nil},
+    {"HipChat",           nil,          display_laptop,  hs.layout.maximized, nil, nil},
+    {"SourceTree",        nil,          display_laptop,  hs.layout.maximized, nil, nil},
+    {"Calendar",          nil,          display_laptop,  hs.layout.maximized, nil, nil},
+    {"Evernote",          nil,          display_laptop,  hs.layout.maximized, nil, nil},
+    {"Spotify",           nil,          display_laptop,  hs.layout.maximized, nil, nil},    
+    {"iTunes",            "iTunes",     display_laptop,  hs.layout.maximized, nil, nil},
+    {"Finder",            nil,          display_laptop, hs.layout.left50,    nil, nil},
+}
+
+-------------------------------- Utility Methods ---------------------------------
+-- Function to toggle an application between being the frontmost app, and being hidden
+function toggle_application(_appName)
+    local app = hs.appfinder.appFromName(_appName)
+    if not app then
+        hs.application.launchOrFocus(_appName)
+        return
+    end
+    local mainwin = app:mainWindow()
+    if mainwin == hs.window.focusedWindow() then
+        mainwin:application():hide()
+    else
+        mainwin:application():activate(true)
+        mainwin:application():unhide()
+        mainwin:focus()
+    end
+end
+
+-- Callback function for application events
+function applicationWatcher(appName, eventType, appObject)
+    if (eventType == hs.application.watcher.activated) then
+        if (appName == "Finder") then
+            -- Bring all Finder windows forward when one gets activated
+            appObject:selectMenuItem({"Window", "Bring All to Front"})
+        end
+    end
+end
+
+-- Callback function for changes in screen layout
+function screensChangedCallback()
+    newNumberOfScreens = #hs.screen.allScreens()
+
+    if lastNumberOfScreens ~= newNumberOfScreens then
+        if newNumberOfScreens == 1 then
+            hs.layout.apply(internal_display)
+        elseif newNumberOfScreens == 3 then
+            hs.layout.apply(triple_display)
+        end
+    end
+
+    lastNumberOfScreens = newNumberOfScreens
+end
+
+-- Reload config automatically
+function reloadConfig()
+    configFileWatcher:stop()
+    configFileWatcher = nil
+
+    appWatcher:stop()
+    appWatcher = nil
+
+    screenWatcher:stop()
+    screenWatcher = nil
+
+    hs.reload()
+end
+
+-- Move the cursor to middle of screen
+function cursorToMiddle()
+  	local win = hs.window.focusedWindow()
+  	local screen = win:screen()
+  	local max = screen:frame()
+
+  	local c = hs.mouse.get()
+	c.x = max.w/2
+	c.y = max.h/2
+	hs.mouse.set(c) -- TODO - Fix this to get to active applications screen
+end
+
+----------------------------- Actual Config Methods ------------------------------
+-- Hotkeys to resize windows absolutely
+hs.hotkey.bind(hyper, '[', function() hs.window.focusedWindow():moveToUnit(hs.layout.left50) end)
+hs.hotkey.bind(hyper, ']', function() hs.window.focusedWindow():moveToUnit(hs.layout.right50) end)
+hs.hotkey.bind(hyper, 'f', function() hs.window.focusedWindow():maximize() end)
+
+hs.hotkey.bind(hyper, 'h', function() hs.window.focusedWindow():focusWindowWest() end)
+hs.hotkey.bind(hyper, 'l', function() hs.window.focusedWindow():focusWindowEast() end)
+hs.hotkey.bind(hyper, 'k', function() hs.window.focusedWindow():focusWindowNorth() end)
+hs.hotkey.bind(hyper, 'j', function() hs.window.focusedWindow():focusWindowSouth() end)
+
+-- Hotkeys to trigger defined layouts
+hs.hotkey.bind(hyper, '1', function() hs.layout.apply(internal_display) end)
+hs.hotkey.bind(hyper, '2', function() hs.layout.apply(triple_display) end)
+
+-- Application hotkeys
+hs.hotkey.bind(hyper, 't', function() toggle_application("iTerm") end)
+hs.hotkey.bind(hyper, 'g', function() toggle_application("Google Chrome") end)
+hs.hotkey.bind(hyper, 'x', function() toggle_application("Xcode") end)
+hs.hotkey.bind(hyper, 'e', function() toggle_application("Evernote") end)
+
+-- Misc hotkeys
+hs.hotkey.bind(hyper, 'r', hs.reload)
+hs.hotkey.bind(hyper, 'y', hs.toggleConsole)
+hs.hotkey.bind(hyper, 'n', function() os.execute("open ~") end)
+hs.hotkey.bind(hyper, 'm', cursorToMiddle)
+hs.hotkey.bind(cmd,   'e', hs.hints.windowHints)
+
+-- Create and start our callbacks
+configFileWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig)
+configFileWatcher:start()
+
+appWatcher = hs.application.watcher.new(applicationWatcher)
+appWatcher:start()
+
+screenWatcher = hs.screen.watcher.new(screensChangedCallback)
+screenWatcher:start()
+
+-- Finally, show a notification that we finished loading the config successfully
+hs.notify.show("Hammerspoon", "", "Locked and loaded!", "")
---
+++ .wsup/Darwin/.hammerspoon/windowConfigs.lua
@@ -0,0 +1,204 @@
+---------------------------------- Core Config -----------------------------------
+hs.window.animationDuration = 0  -- Disable window animations (janky for iTerm)
+
+----------------------------------- Variables ------------------------------------
+--
+--		saved.win 		The window last moved.
+--		saved.winframe The frame for the window before moving.
+--
+saved = {}
+saved.win = {}
+saved.winframe = {}
+local hyper = {"⌘", "⌥", "⌃", "⇧"}
+
+----------------------------------- Methods ------------------------------------
+function returnLast()
+	if(saved.win ~= {}) then
+		saved.win:setFrame(saved.winframe)
+	end
+end
+
+function nudgeRight()
+   local win = hs.window.focusedWindow()
+   local f = win:frame()
+   saved.win = win
+   saved.winframe = saved.win:frame()
+   f.x = f.x + 10
+   win:setFrame(f)
+end
+
+function nudgeLeft()
+   local win = hs.window.focusedWindow()
+   local f = win:frame()
+   saved.win = win
+   saved.winframe = saved.win:frame()
+   f.x = f.x - 10
+   win:setFrame(f)
+end
+
+function nudgeUp()
+   local win = hs.window.focusedWindow()
+   local f = win:frame()
+   saved.win = win
+   saved.winframe = saved.win:frame()
+   f.y = f.y - 10
+   win:setFrame(f)
+end
+
+function nudgeDown()
+   local win = hs.window.focusedWindow()
+   local f = win:frame()
+   saved.win = win
+   saved.winframe = saved.win:frame()
+   f.y = f.y + 10
+   win:setFrame(f)
+end
+
+function leftThirds()
+   saved.win = hs.window.focusedWindow()
+   saved.winframe = saved.win:frame()
+	hs.grid.set(saved.win, {x = 0, y = 0, w = 1, h = 3}, hs.screen.mainScreen())
+end
+
+function rightThirds()
+   saved.win = hs.window.focusedWindow()
+   saved.winframe = saved.win:frame()
+	hs.grid.set(hs.window.focusedWindow(), {x = 1, y = 0, w = 2, h = 3}, hs.screen.mainScreen())
+end
+
+--
+-- Function:         snapWindow
+--
+-- Description:      Move the current window to the closes area in the
+--                   window matrix.
+--
+function snapWindow()
+	saved.win = hs.window.focusedWindow()
+	saved.winframe = saved.win:frame()
+	hs.grid.snap(hs.window.focusedWindow())
+end
+
+--
+-- Function:		setgrid
+--
+-- Description:	This function sets the current window to
+--						the specified grid location.
+--
+function setgrid( sx, sy, sw, sh)
+   saved.win = hs.window.focusedWindow()
+   saved.winframe = saved.win:frame()
+	hs.grid.set(hs.window.focusedWindow(), {x = sx, y = sy, w = sw, h = sh}, hs.screen.mainScreen())
+end
+
+--
+-- Global Variables
+--
+--		expose.wins 		A list of windows exposed
+-- 		expose.size 		The number of windows in expose.wins
+-- 		expose.winsSize		The frame for each window exposed
+--								original location and size.
+--		expose.sX			The size of the grid in the X for expose
+--		expose.sY 			The size of the grid in the Y for expose
+--
+expose = {}
+expose.wins = {}
+expose.size = 0
+expose.winsSize = {}
+expose.sX = 0
+expose.sY = 0
+
+--
+-- Function:		exposeStart
+--
+-- Description:	This function starts a window expose. It
+--              gets the name of the application to expose.
+--
+function exposeStart( appName )
+	saveWidth = hs.grid.GRIDWIDTH
+	saveHeight = hs.grid.GRIDHEIGHT
+	app = hs.appfinder.appFromName( appName )
+	app:activate(true)
+	expose.wins = app:allWindows()
+	expose.size = 0
+	for Index, win in pairs( expose.wins ) do
+		expose.size = expose.size + 1
+		expose.winsSize[ Index ] = win:frame()
+	end
+	if expose.size == 1 then
+		expose.wins[1]:focus()
+	else
+		sX = math.ceil(math.sqrt(expose.size))
+		sY = math.ceil(expose.size / sX)
+		hs.grid.GRIDWIDTH = sX
+		hs.grid.GRIDHEIGHT = sY
+		for index, win in pairs(expose.wins) do
+			index = index - 1
+			iY = math.floor(index / sX)
+			iX = index - (iY * sX)
+			hs.grid.set(win, { x=iX, y=iY, w=1, h=1}, hs.screen.mainScreen())
+		end
+		expose.sX = sX
+		expose.sY = sY
+		hs.grid.GRIDWIDTH = saveWidth
+		hs.grid.GRIDHEIGHT = saveHeight
+	end
+	print(expose.size)
+end
+
+--
+-- Function:		exposeStop
+--
+-- Description:	This function puts all the windows back to
+--              their original location and focus' on the
+--              window given by it's col, row address in the
+--              expose.
+--
+function exposeStop( col, row )
+	exWin = ""
+	for Index, win in pairs( expose.wins ) do
+		win:setFrame(expose.winsSize[ Index ])
+		if( Index == (expose.sX*row + col + 1)) then
+			exWin = win
+		end
+	end
+	exWin:focus()
+end
+
+--
+-- Function:        runningApps
+--
+-- Description:     This function lists all applications running
+--                  and visible from the AppBar.
+--
+function runningApps( )
+    apps = hs.application.runningApplications()
+    for index, app in pairs( apps ) do
+        if app:kind() == 1 then
+            wins = app:visibleWindows()
+        	size = 0
+        	for Index, win in pairs( wins ) do
+        		size = size + 1
+        	end
+        	if size >= 1 then
+                print( app:title() )
+            end
+        end
+	end
+end
+
+--- osexec(command[, with_user_env]) -> output, status, type, rc
+--- Function
+--- Runs a shell command and returns stdout as a string (may include a trailing newline), followed by true or nil indicating if the command completed successfully, the exit type ("exit" or "signal"), and the result code.
+---
+---  If `with_user_env` is `true`, then invoke the user's default shell as an interactive login shell in which to execute the provided command in order to make sure their setup files are properly evaluated so extra path and environment variables can be set.  This is not done, if `with_user_env` is `false` or not provided, as it does add some overhead and is not always strictly necessary.
+function osexec(command, user_env)
+    local f
+    if user_env then
+        f = io.popen(os.getenv("SHELL").." -l -i -c \""..command.."\"", 'r')
+    else
+        f = io.popen(command, 'r')
+    end
+    local s = f:read('*a')
+    local status, exit_type, rc = f:close()
+    return s, status, exit_type, rc
+end
