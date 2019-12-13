995a441e8c7a81a2a4902ec8e4c74bbe43cc8db6
Deepu Mukundan <deepu.mukundan@workday.com>
2019-12-13 10:46:18 -0800

    Updates to scripts and new tools

--- .wsup/Darwin/.hammerspoon/init.lua
+++ .wsup/Darwin/.hammerspoon/init.lua
@@ -5,16 +5,19 @@ local layoutConfigs = require "layoutConfigs"
 local windowConfigs = require "windowConfigs"
 local utilityFunctions = require "utilityFunctions"
 
-local alt = {"⌥"}
-local cmd = {"⌘"}
+-- local alt = {"⌥"}
+-- local cmd = {"⌘"}
 
 -- Hotkeys to resize windows absolutely
 hyper:bind({}, '[', nil, function() hs.window.focusedWindow():moveToUnit(hs.layout.left50) end)
 hyper:bind({}, ']', nil, function() hs.window.focusedWindow():moveToUnit(hs.layout.right50) end)
-hyper:bind({}, 'f', nil, function() hs.window.focusedWindow():maximize() end)
+hyper:bind({}, 'z', nil, function() hs.window.focusedWindow():maximize() end)
 
 -- Hotkeys to trigger defined layouts & move
-hyper:bind({}, '0', nil, function() windowConfigs.moveWindow() end)
+hyper:bind({}, '0', nil, 
+function() 
+    windowConfigs.moveWindow() 
+end)
 
 hyper:bind({}, '1', nil,
 function()
@@ -28,22 +31,21 @@ function()
     hs.notify.show("Display Switch", "", "Triple Display Configuration", "")
 end)
 
--- Application hotkeys
-hyper:bind({}, 't', nil, function() toggle_application("iTerm") end)
-hyper:bind({}, 'g', nil, function() toggle_application("Google Chrome") end)
-hyper:bind({}, 'x', nil, function() toggle_application("Xcode") end)
-hyper:bind({}, 'c', nil, function() toggle_application("Code") end)
-hyper:bind({}, 'o', nil, function() toggle_application("Outlook") end)
-hyper:bind({}, 'l', nil, function() toggle_application("Slack") end)
-hyper:bind({}, 's', nil, function() toggle_application("Sourcetree") end)
-
--- Misc hotkeys
-hyper:bind({}, 'r', nil, hs.reload)
-hyper:bind({}, 'y', nil, hs.toggleConsole)
-hyper:bind({}, 'f', nil, function() os.execute("open ~") end)
-hyper:bind({}, 'm', nil, cursorToMiddle)
-hyper:bind({}, 'u', nil, mouseHighlight)
+-- Hotkeys
 hyper:bind({}, 'e', nil, hs.hints.windowHints)
+hyper:bind({}, 'f', nil, function() os.execute("open ~") end)
+hyper:bind({}, 'g', nil, function() toggle_application("Google Chrome") end)
+hyper:bind({}, 'k', nil, function() toggle_application("ReadKit") end)
+hyper:bind({}, 'l', nil, function() toggle_application("Slack") end)
+hyper:bind({}, 'm', nil, cursorToMiddle)
+hyper:bind({}, 'o', nil, function() toggle_application("Outlook") end)
+hyper:bind({}, 'r', nil, hs.reload)
+hyper:bind({}, 's', nil, function() toggle_application("Sourcetree") end)
+hyper:bind({}, 't', nil, function() toggle_application("iTerm") end)
+hyper:bind({}, 'u', nil, mouseHighlight)
+hyper:bind({}, 'v', nil, function() toggle_application("Code") end)
+hyper:bind({}, 'x', nil, function() toggle_application("Xcode") end)
+hyper:bind({}, 'y', nil, hs.toggleConsole)
 
 -- Trigger another keystroke
 --- hyper:bind({}, 'l', nil, function() hs.eventtap.keyStroke({'⌃'}, 'l'); k.triggered = true; end)
